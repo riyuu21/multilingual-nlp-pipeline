e3ddad7427ddf0ac0638be23b98351a699dc4361
Riyansh <riyannnsh21@gmail.com>
2026-03-22 21:15:45 +0530

    15.75th push
    some fixes

--- frontend/src/components/ResultCards.jsx
+++ frontend/src/components/ResultCards.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 const LANGUAGE_NAMES = {
     af: "Afrikaans", sq: "Albanian", ar: "Arabic", az: "Azerbaijani",
